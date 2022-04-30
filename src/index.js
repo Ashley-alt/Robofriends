7fc0afe0d87800a47cdc8e4d22a93be58f944eb2
ashley-alt <a.olubanjo1@gmail.com>
2022-04-30 15:29:54 +0100

    Styling to card components

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ import 'tachyons';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card/>
+    <div>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
   </React.StrictMode>
 );
 
